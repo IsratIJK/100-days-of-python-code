5a4d817c4872b9bb296a092c39b6d52b59ee7e28
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-19 11:41:55 +0600

    New line charecter

--- Day 1/Ex1.py
+++ Day 1/Ex1.py
@@ -1,3 +1,4 @@
 print("Day 1 - Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
+print('print("what to print")') # we have to use double quotes to contain a single quote in a line
