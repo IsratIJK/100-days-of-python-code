e522898621af567da6dd5a2773dea22240f8279e
IsratIJK <iptu.jahan.amin@gmail.com>
2021-01-27 21:15:09 +0600

    Project band name generator

---
+++ Day 1/Project.py
@@ -0,0 +1,4 @@
+print("Hello this is my first project in 100 days of python\n")
+city = input("The name of your city: \n")
+pet = input("The name of your pet: \n")
+band = print("The band name: "+city+" "+pet)
